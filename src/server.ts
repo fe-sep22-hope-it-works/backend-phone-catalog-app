934a2e8a7983a26877a7bfdc7849684a38bbad8d
Illia Skladnik <scladnik@gmail.com>
2023-01-05 13:36:19 +0200

    Phones Route created

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,24 @@
 import express from 'express';
 import cors from 'cors';
+
+
 // import * as goodController from "./controllers/goods";
 // import * as colorsController from "./controllers/colors";
 import serverless from 'serverless-http';
+import { getAll } from './services/phones';
 
 const router = express.Router();
 
 const app = express();
 
 app.use(cors());
+
+router.get('/phones', async (req, res) => {
+  const allPhones = await getAll();
+  res.json(allPhones);
+
+})
+
 
 router.get('/', (req, res) => {
     res.json({
---
+++ src/services/phones.ts
@@ -0,0 +1,10 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const filePath = path.resolve('public/api', 'phones.json');
+
+export async function getAll() {
+  const data = await fs.readFile(filePath, 'utf-8');
+
+  return JSON.parse(data);
+}

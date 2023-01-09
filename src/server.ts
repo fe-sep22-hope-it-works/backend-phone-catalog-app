9691ffe46a122691bb52f851ff568481887ef59d
Andrey Olin <crazy106ras@gmail.com>
2023-01-09 19:03:21 +0200

    cors activated (#5)

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 import serverless from 'serverless-http';
 import { phonesRouter } from './routes/phones';
 import { publicRouter } from './routes/public';
@@ -8,7 +8,7 @@ const router = express.Router();
 const app = express();
 const API_PATH = '/.netlify/functions/server';
 
-// app.use(cors());
+app.use(cors());
 app.use(`${API_PATH}/phones`, phonesRouter);
 app.use(`${API_PATH}/public`, publicRouter);
 app.use(API_PATH, router);

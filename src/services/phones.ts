583e04a210a274aa4f608988dc8af4fa9dc0dad4
Illia Skladnik <scladnik@gmail.com>
2023-01-12 12:26:05 +0200

    Methods fixed

--- src/services/phones.ts
+++ src/services/phones.ts
@@ -77,6 +77,8 @@ export async function getNewestPhones() {
   const newestPhones = allPhones
     .filter((phone: Phone) => (phone.year === maxYear));
 
+  newestPhones.sort((a: Phone, b: Phone) => b.price - a.price);
+
   return newestPhones;
 }
 
